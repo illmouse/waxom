40f1e7cf75806043cb7ec3471912bd29fc363b30
illmouse <nadenisov@gmail.com>
2017-05-29 17:20:20 +0300

    Making structure pretty.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,31 @@
 var gulp 				= require('gulp'),
 		sass 				= require('gulp-sass'),
-		haml 				= require('gulp-ruby-haml'),
+    pug      		= require('gulp-pug'),
 		browserSync = require('browser-sync'),
 		concat      = require('gulp-concat'),
-    uglify      = require('gulp-uglifyjs'),
     notify      = require('gulp-notify');
 
 
 gulp.task('sass', function() {
-	return gulp.src('app/sass/*.sass')
+	return gulp.src('src/sass/*.sass')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('app/css'))
+		.pipe(gulp.dest('src/css'))
 		.pipe(browserSync.reload({stream:true}))
 		.pipe(notify('CSS changed.'))
 });
 
-gulp.task('haml', function() {
-	return gulp.src('app/haml/*.haml')
-		.pipe(haml())
-		.pipe(gulp.dest('app/'))
+// gulp.task('haml', function() {
+// 	return gulp.src('src/haml/*.haml')
+// 		.pipe(haml())
+// 		.pipe(gulp.dest('src/'))
+// 		.pipe(browserSync.reload({stream: true}))
+// 		.pipe(notify('HTML changed.'))
+// });
+
+gulp.task('pug', function() {
+	return gulp.src('src/pug/*.pug')
+		.pipe(pug())
+		.pipe(gulp.dest('src/'))
 		.pipe(browserSync.reload({stream: true}))
 		.pipe(notify('HTML changed.'))
 });
@@ -26,23 +33,23 @@ gulp.task('haml', function() {
 gulp.task('browserSync', function() {
 	browserSync({
 		server: {
-			baseDir: 'app'
+			baseDir: 'src'
 		},
 		notify: false
 	});
 });
 
 gulp.task('scripts', function() {
-    return gulp.src('app')
+    return gulp.src('src')
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('app/js'));
+        .pipe(gulp.dest('src/js'));
 });
 
-gulp.task('watch', ['browserSync', 'sass', 'haml'], function() {
-	gulp.watch('app/sass/*.sass', ['sass']);
-	gulp.watch('app/haml/*.haml', ['haml']);
-	gulp.watch('app/*.html', browserSync.reload);
+gulp.task('watch', ['browserSync', 'sass', 'pug'], function() {
+	gulp.watch('src/sass/*.sass', ['sass']);
+	gulp.watch('src/pug/*.pug', ['pug']);
+	gulp.watch('src/*.html', browserSync.reload);
 });
 
 gulp.task('default', ['watch']);
