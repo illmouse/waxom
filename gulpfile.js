9dd3ff882b1923ce98155cc85546529234d9718e
illmouse <nadenisov@gmail.com>
2017-06-06 19:07:19 +0300

    Fixes for html and gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -7,25 +7,11 @@ var gulp 					= require('gulp'),
     autoprefixer	= require('gulp-autoprefixer'),
     sourcemaps 		= require('gulp-sourcemaps');
 
+var pug_in = 'src/pug/*.pug'
+var pug_out = 'src'
 
-gulp.task('sass', function() {
-	return gulp.src('src/sass/*.sass')
-		.pipe(sourcemaps.init())
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer('> 5%', 'last 2 version'))
-		.pipe(sourcemaps.write(''))
-		.pipe(gulp.dest('src/css'))
-		.pipe(browserSync.reload({stream:true}))
-		.pipe(notify({message: 'CSS changed.', onLast: true}))
-});
-
-gulp.task('pug', function() {
-	return gulp.src('src/pug/*.pug')
-		.pipe(pug())
-		.pipe(gulp.dest('src/'))
-		.pipe(browserSync.reload({stream: true}))
-		.pipe(notify('HTML changed.'))
-});
+var sass_in = 'src/sass/*.sass'
+var sass_out = 'src/css'
 
 gulp.task('browserSync', function() {
 	browserSync({
@@ -36,17 +22,44 @@ gulp.task('browserSync', function() {
 	});
 });
 
-gulp.task('scripts', function() {
-    return gulp.src('src')
-        .pipe(concat('libs.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('src/js'));
+gulp.task('sass', function() {
+	return gulp.src(sass_in)
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer('> 5%', 'last 2 version'))
+		.pipe(sourcemaps.write(''))
+		.pipe(gulp.dest(sass_out))
+		// .pipe(browserSync.reload({stream: true}))
+		.pipe(notify({message: 'CSS changed.', onLast: true}))
+});
+
+gulp.task('sass-reload', ['sass'], function() {
+    browserSync.reload({stream: true});
+});
+
+
+gulp.task('pug', function() {
+	return gulp.src(pug_in)
+		.pipe(pug())
+		.pipe(gulp.dest(pug_out))
+		.pipe(notify('HTML changed.'))
+});
+
+gulp.task('pug-reload', ['pug'], function() {
+    browserSync.reload({stream: true});
 });
 
 gulp.task('watch', ['browserSync', 'sass', 'pug'], function() {
-	gulp.watch('src/sass/*.sass', ['sass']);
-	gulp.watch('src/pug/*.pug', ['pug']);
-	// gulp.watch('src/*.html', browserSync.reload);
+	gulp.watch(sass_in, ['sass-reload']);
+	gulp.watch(pug_in, ['pug-reload']);
 });
 
 gulp.task('default', ['watch']);
+
+
+// gulp.task('scripts', function() {
+//     return gulp.src('src')
+//         .pipe(concat('libs.min.js'))
+//         .pipe(uglify())
+//         .pipe(gulp.dest('src/js'));
+// });
